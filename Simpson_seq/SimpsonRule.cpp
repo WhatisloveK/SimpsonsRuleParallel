2f6c9eb6a4b3220da4dd8124b13ae8b25b290f97
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2020-05-28 15:24:18 +0300

    working well

--- Simpson_seq/SimpsonRule.cpp
+++ Simpson_seq/SimpsonRule.cpp
@@ -6,14 +6,16 @@ void SimpsonRule::set_step(double xmin, double xmax, long long n) {
 	this->step = (xmax - xmin) / n;
 }
 
+
 double SimpsonRule::seq_simpson(double (*func)(double x)) {
-	double _sum4 = 0, _sum2 = 0;
-	for (long long i = 1; i < 2 * n; i += 2) {
-		//for even numbers
-		_sum4 += func(xmin + i * step);
-		//for uneven
-		_sum2 += func(xmin + (i + 1) * step);
+	double simpson_integral = 0;
+	for (long long i = 0; i < n; i ++) {
+
+		const double x1 = xmin + step * i;
+		const double x2 = xmin + (i + 1) * step;
+
+		simpson_integral += (x2 - x1) / 6.0 * (func(x1) + 4.0 * func(0.5 * (x1 + x2)) + func(x2));
 	}
-	double res = step / 3 * (func(xmin) + 4 * _sum4 + 2 * _sum2 - func(xmax));
-	return res;
+	
+	return simpson_integral;
 }
