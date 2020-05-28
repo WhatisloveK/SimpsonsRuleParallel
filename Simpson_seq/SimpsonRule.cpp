5d7658dc0606f2339f548cd7619d29d59d5d5bf2
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2020-05-28 15:22:03 +0300

    base simpson

--- Simpson_seq/SimpsonRule.cpp
+++ Simpson_seq/SimpsonRule.cpp
@@ -6,31 +6,14 @@ void SimpsonRule::set_step(double xmin, double xmax, long long n) {
 	this->step = (xmax - xmin) / n;
 }
 
-double simpsonIntegral(double a, double b, int n, const std::function<double(double)>& f) {
-	const double width = (b - a) / n;
-
-	double simpson_integral = 0;
-	for (int step = 0; step < n; step++) {
-		const double x1 = a + step * width;
-		const double x2 = a + (step + 1) * width;
-
-		simpson_integral += (x2 - x1) / 6.0 * (f(x1) + 4.0 * f(0.5 * (x1 + x2)) + f(x2));
-	}
-
-	return simpson_integral;
-}
 double SimpsonRule::seq_simpson(double (*func)(double x)) {
-	double simpson_integral = 0;
-	for (long long i = 0; i < n; i ++) {
+	double _sum4 = 0, _sum2 = 0;
+	for (long long i = 1; i < 2 * n; i += 2) {
 		//for even numbers
 		_sum4 += func(xmin + i * step);
 		//for uneven
 		_sum2 += func(xmin + (i + 1) * step);
-		const double x1 = xmin + step * i;
-		const double x2 = xmin + (i + 1) * step;
-
-		simpson_integral += (x2 - x1) / 6.0 * (func(x1) + 4.0 * func(0.5 * (x1 + x2)) + func(x2));
 	}
-	
-	return simpson_integral;
+	double res = step / 3 * (func(xmin) + 4 * _sum4 + 2 * _sum2 - func(xmax));
+	return res;
 }
